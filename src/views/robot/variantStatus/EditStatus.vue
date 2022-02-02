448099e45c470d3562ff00275a04cafc303d5ced
alireza <alireza.mastery@gmail.com>
2022-02-02 23:29:51 +0330

    added variant edit status view

--- src/views/robot/variantStatus/EditStatus.vue
+++ src/views/robot/variantStatus/EditStatus.vue
@@ -1,48 +1,70 @@
 <template>
   <div class="q-ma-md q-pa-sm">
 
-    <div class="text-h5">{{ $t('products.brands') }}</div>
-    <div class="q-ma-sm row q-gutter-md">
-      <q-btn
+    <div class="text-h5 q-mb-sm">{{ $t('products.brands') }}</div>
+    <div class="row q-col-gutter-sm">
+      <div
         v-for="brand in brands"
         :key="brand.id"
-        color="green"
-        size="xl"
-        @click="handleBrandSelect(brand.id)"
+        class="col-xs-12 col-sm-6 col-md-4 col-lg-2 col-xl-1"
       >
-        {{ brand.title }}
-      </q-btn>
+        <q-btn
+          color="green"
+          size="xl"
+          class="full-width"
+          @click="handleBrandSelect(brand.id)"
+        >
+          {{ brand.title }}
+        </q-btn>
+      </div>
+
     </div>
 
     <template v-if="actualProducts.length > 0">
       <q-separator class="q-my-lg" inset/>
-      <div class="text-h5">{{ $t('products.actualProducts') }}</div>
-      <div class="q-ma-sm row q-gutter-sm">
-        <q-btn
+      <div class="text-h5 q-mb-sm">{{ $t('products.actualProducts') }}</div>
+      <div class="row q-col-gutter-sm">
+        <div
           v-for="actual in actualProducts"
           :key="actual.id"
-          color="primary"
-          size="lg"
-          @click="handleActualProductSelect(actual.id)"
+          class="col-xs-12 col-sm-6 col-md-4 col-lg-2"
         >
-          {{ actual.title }}
-        </q-btn>
+          <q-btn
+            color="primary"
+            size="lg"
+            class="full-width full-height"
+            @click="handleActualProductSelect(actual.id)"
+          >
+            {{ actual.title }}
+          </q-btn>
+        </div>
+
       </div>
     </template>
 
     <template v-if="variants.length > 0">
-      <q-separator class="q-my-lg" inset/>
-      <div class="text-h5">{{ $t('products.variants') }}</div>
-      <div class="q-ma-sm row q-gutter-sm">
-        <q-btn
+      <q-separator class="q-my-md" inset/>
+      <div class="text-h5 q-mb-sm">{{ $t('products.variants') }}</div>
+      <div class="row q-col-gutter-sm">
+        <div
           v-for="variant in variants"
           :key="variant.id"
-          color="primary"
-          flat
+          class="col-xs-12 col-sm-6 col-lg-4"
         >
-          {{ `${variant.product.title} ${variant.selector_values[0].value}` }}
-        </q-btn>
+          <q-btn
+            color="primary"
+            flat
+            @click="handleVariantSelect(variant.id)"
+          >
+            {{ `${variant.product.title} ${variant.selector_values[0].value}` }}
+          </q-btn>
+        </div>
       </div>
+    </template>
+
+    <template v-if="variant">
+      <q-separator class="q-my-lg" inset/>
+      <Variant :variant="variant"/>
     </template>
 
   </div>
@@ -51,12 +73,15 @@
 <script setup>
 import {ref} from 'vue'
 import urls from 'src/urls'
+import Variant from './Variant.vue'
 
 import {axiosInstance} from 'src/boot/axios'
+import {notifyErrors} from 'src/composables/notif'
 
 const brands = ref(null)
 const actualProducts = ref([])
 const variants = ref([])
+const variant = ref(null)
 
 axiosInstance.get(urls.brandsAll)
   .then(res => {
@@ -71,6 +96,7 @@ function handleBrandSelect(brandId) {
       console.log('actuala', res.data)
       actualProducts.value = res.data
       variants.value = []
+      variant.value = null
     })
 }
 
@@ -80,6 +106,20 @@ function handleActualProductSelect(id) {
     .then(res => {
       console.log('handleActualProductSelect', res.data)
       variants.value = res.data.variants
+      variant.value = null
+    })
+}
+
+
+function handleVariantSelect(id) {
+  const url = urls.variantDigiData + id + '/'
+  axiosInstance.get(url)
+    .then(res => {
+      console.log('handleVariantSelect')
+      variant.value = res.data
+    })
+    .catch(err => {
+      notifyErrors(err.response.data)
     })
 }
 
---
+++ src/views/robot/variantStatus/Variant.vue
@@ -0,0 +1,311 @@
+<template>
+  <q-card bordered>
+    <q-card-section>
+      <!--  First Row START -->
+      <div class="row items-center">
+        <div class="col-xs-12 col-sm-12 col-lg-3 text-h6">
+          {{ variant.product.title }}
+        </div>
+        <div class="col-xs-12 col-sm-12 col-lg-3 text-body1">
+          {{ variant.selector_values[0].value }}
+        </div>
+        <div class="col-xs-12 col-sm-12 col-lg-3 text-body1">
+          {{ variant.dkpc }}
+        </div>
+        <div class="col-xs-12 col-sm-12 col-lg-3">
+          <q-toggle
+            v-model="digiStatus"
+            :label="$t('products.activeInDigi')"
+            @update:model-value="handleDigiStatusUpdate"
+            checked-icon="check"
+            unchecked-icon="clear"
+            size="lg"
+          >
+            <template v-slot:default v-if="loadingDigiStatus">
+              <q-circular-progress indeterminate/>
+            </template>
+          </q-toggle>
+        </div>
+      </div>
+      <!--  First Row END -->
+    </q-card-section>
+
+    <q-card-section>
+      <!--  Second Row START -->
+      <div class="row items-center">
+        <div class="col-xs-12 col-md-4">
+          <div class="row items-center justify-between">
+            <div class="col-6">
+              <q-btn
+                outline
+                disable
+              >
+                {{ `${$t('products.ordered')}: ${variant.reserved}` }}
+              </q-btn>
+            </div>
+            <div class="col-6">
+              <q-btn
+                outline
+                disable
+              >
+                {{ `${$t('products.inDigiStock')}: ${variant['warehouse_stock']}` }}
+              </q-btn>
+            </div>
+          </div>
+        </div>
+        <!--  Digi data Inputs -->
+        <div class="col-xs-12 col-sm-6 col-md-4">
+          <div class="row q-col-gutter-sm">
+            <div class="col-xs-12 col-sm-6">
+              <q-input
+                v-model="newPrice"
+                :label="$t('general.priceRial')"
+                :disable="loadingDigiData"
+                filled
+              />
+            </div>
+            <div class="col-xs-12 col-sm-6">
+              <q-input
+                v-model="newStock"
+                :label="$t('products.inventory')"
+                :disable="loadingDigiData"
+                filled
+              />
+            </div>
+          </div>
+        </div>
+        <!--  Digi data Buttons -->
+        <div v-show="showBtnDigi" class="col-xs-12 col-sm-6 col-md-4 col-lg-2">
+          <div v-if="loadingDigiData" class="row justify-center">
+            <q-circular-progress
+              indeterminate
+              color="red"
+              size="md"
+            />
+          </div>
+          <div v-else class="row q-col-gutter-sm justify-around">
+            <div class="col-xs-6 col-sm-3 col-lg-2">
+              <q-btn
+                color="green"
+                class="q-mx-sm"
+                icon="done"
+                no-wrap
+                @click="handleDigiDataUpdate"
+              >
+              </q-btn>
+            </div>
+            <div class="col-xs-6 col-sm-3 col-lg-2">
+              <q-btn
+                color="red"
+                class="q-mx-sm"
+                icon="block"
+                no-wrap
+                @click="revertDigiChange"
+              >
+              </q-btn>
+            </div>
+          </div>
+        </div>
+      </div>
+      <!--  Second Row END -->
+    </q-card-section>
+
+    <q-separator/>
+
+    <q-card-section>
+      <!--  Third Row START -->
+      <div class="row items-center">
+        <!--  Robot Status -->
+        <div class="col-xs-12 col-sm-12 col-md-4">
+          <q-toggle
+            v-model="robotStatus"
+            :label="$t('products.robotActive')"
+            checked-icon="bolt"
+            unchecked-icon="power_off"
+            @update:model-value="handleRobotStatusUpdate"
+            size="lg"
+          >
+            <template v-slot:default v-if="loadingDigiStatus">
+              <q-circular-progress indeterminate/>
+            </template>
+          </q-toggle>
+        </div>
+        <!--  Persia Atlas Data Input -->
+        <div class="col-xs-12 col-sm-6 col-md-3 col-lg-2">
+          <q-input
+            v-model="newPriceMin"
+            :label="$t('general.priceMinToman')"
+            filled
+          />
+        </div>
+        <!--  Persia Atlas Data Buttons -->
+        <div v-show="showBtnAtlas" class="col-xs-12 col-sm-6 col-md-4 col-lg-2">
+          <q-circular-progress
+            v-if="loadingRobotStatus"
+            indeterminate
+            color="red"
+          />
+          <div v-else class="row q-col-gutter-sm justify-around">
+            <div class="col-xs-6 col-sm-3 col-lg-2">
+              <q-btn
+                color="green"
+                icon="done"
+                class="q-mx-sm"
+                @click="handleAtlasUpdate"
+              />
+            </div>
+            <div class="col-xs-6 col-sm-3 col-lg-2">
+              <q-btn
+                color="red"
+                icon="block"
+                class="q-mx-sm"
+                @click="revertAtlasDataChange"
+              />
+            </div>
+          </div>
+        </div>
+      </div>
+      <!--  Third Row END -->
+    </q-card-section>
+  </q-card>
+</template>
+
+<script setup>
+import {ref, computed, watch} from 'vue'
+import {formatIntNumber, removeCommas} from 'src/composables/number-tools'
+import {axiosInstance} from 'src/boot/axios'
+import urls from 'src/urls'
+import {notifyErrors, notifyMessage} from '../../../composables/notif'
+import {useI18n} from 'vue-i18n'
+
+const {t} = useI18n()
+
+const props = defineProps({
+  variant: Object,
+})
+
+const initialPriceMin = ref(props.variant['price_min'].toString())
+const newPriceMin = ref(formatIntNumber(props.variant['price_min'].toString()))
+const initialPrice = ref(props.variant['price'].toString())
+const initialStock = ref(props.variant['our_stock'].toString())
+const newPrice = ref(formatIntNumber(props.variant['price'].toString()))
+const newStock = ref(props.variant['our_stock'].toString())
+
+const robotStatus = ref(props.variant['is_active'])
+const digiStatus = ref(props.variant['is_digi_active'])
+
+const loadingDigiStatus = ref(false)
+const loadingDigiData = ref(false)
+const loadingRobotStatus = ref(false)
+const loadingAtlasData = ref(false)
+
+const showBtnDigi = computed(() => {
+  const commaRemovedNewPrice = removeCommas(newPrice.value)
+  return initialPrice.value !== commaRemovedNewPrice || initialStock.value !== newStock.value
+})
+const showBtnAtlas = computed(() => {
+  const commaRemovedNewPriceMin = removeCommas(newPriceMin.value)
+  return initialPriceMin.value !== commaRemovedNewPriceMin
+})
+
+watch(newPriceMin, (newVal) => newPriceMin.value = formatIntNumber(newVal))
+watch(newPrice, (newVal) => newPrice.value = formatIntNumber(newVal))
+
+
+function revertDigiChange() {
+  newPrice.value = initialPrice.value
+  newStock.value = initialStock.value
+}
+
+function revertAtlasDataChange() {
+  newPriceMin.value = initialPriceMin.value
+}
+
+function handleDigiStatusUpdate(event) {
+  loadingDigiStatus.value = true
+  const data = {
+    'dkpc': props.variant.dkpc,
+    'is_active': !!event,
+  }
+  digiStatus.value = !!event
+  axiosInstance.post(urls.updateVariantStatus, data)
+    .then(res => {
+      console.log('handleDigiStatusUpdate | res', res)
+      console.log('handleDigiStatusUpdate | digiStatus', digiStatus.value)
+      notifyMessage('positive', t('general.snack.saveSuccess'))
+    })
+    .catch(err => {
+      console.log('handleDigiStatusUpdate | error', err.response?.data)
+      digiStatus.value = !event
+      notifyErrors(err.response.data)
+    })
+    .finally(() => loadingDigiStatus.value = false)
+}
+
+function handleDigiDataUpdate() {
+  loadingDigiData.value = true
+  const data = {
+    'dkpc': props.variant.dkpc,
+    'price': removeCommas(newPrice.value),
+    'our_stock': newStock.value,
+  }
+  axiosInstance.post(urls.updateVariantData, data)
+    .then(res => {
+      console.log('handleDigiDataUpdate | res', res)
+      const data = res.data
+      initialPrice.value = data['price_sale_latin'].toString()
+      initialStock.value = data['marketplace_seller_stock_latin'].toString()
+      newPrice.value = initialPrice.value
+      newStock.value = initialStock.value
+      notifyMessage('positive', t('general.snack.saveSuccess'))
+    })
+    .catch(err => {
+      console.log('handleDigiDataUpdate | error', err)
+      notifyErrors(err.response.data)
+    })
+    .finally(() => loadingDigiData.value = false)
+}
+
+function handleRobotStatusUpdate(event) {
+  loadingRobotStatus.value = true
+  const data = {
+    'dkpc': props.variant.dkpc,
+    'is_active': !!event,
+  }
+  console.log('data', data)
+  robotStatus.value = !!event
+  axiosInstance.patch(`${urls.variants}${props.variant.id}/`, data)
+    .then(res => {
+      console.log('handleRobotStatusUpdate | res', res)
+      robotStatus.value = res.data.is_active
+      notifyMessage('positive', t('general.snack.saveSuccess'))
+    })
+    .catch(err => {
+      console.log('handleRobotStatusUpdate | error', err)
+      robotStatus.value = !event
+      notifyErrors(err.response.data)
+    })
+    .finally(() => loadingRobotStatus.value = false)
+}
+
+function handleAtlasUpdate() {
+  loadingAtlasData.value = true
+  const data = {
+    'dkpc': props.variant.dkpc,
+    'price_min': parseInt(removeCommas(newPriceMin.value)),
+  }
+  axiosInstance.patch(`${urls.variants}${props.variant.id}/`, data)
+    .then(res => {
+      console.log('handleAtlasUpdate | res', res)
+      initialPriceMin.value = res.data.price_min.toString()
+      newPriceMin.value = res.data.price_min.toString()
+      notifyMessage('positive', t('general.snack.saveSuccess'))
+    })
+    .catch(err => {
+      console.log('handleAtlasUpdate | error', err)
+      notifyErrors(err.response.data)
+    })
+    .finally(() => loadingAtlasData.value = false)
+}
+
+</script>
